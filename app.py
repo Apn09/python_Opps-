94cc8ae759c880759e0149ae1c5a288be50f6b61
karthi apn <karthiapn@karthis-MacBook-Air.local>
2026-03-08 14:13:14 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+print("Hello, World!")
+print ("Welcome to Python programming.")
+print ("This is a simple Python program.")
+print("10>5 is", 10>5)
+for i in range(5):
+    print("Iteration", i)       
+
+
+def greet(name):
+    print("Hello,", name)
+greet("APN")
---
+++ python.py
@@ -0,0 +1,78 @@
+from abc import ABC, abstractmethod
+
+# Abstraction
+class DeliveryService(ABC):
+
+    def __init__(self, customer):
+        self.customer = customer
+
+    @abstractmethod
+    def deliver(self):
+        pass
+
+
+# Inheritance
+class BikeDelivery(DeliveryService):
+
+    def deliver(self):
+        print(f"Order delivered to {self.customer} using Bike")
+
+
+class CarDelivery(DeliveryService):
+
+    def deliver(self):
+        print(f"Order delivered to {self.customer} using Car")
+
+
+# Encapsulation
+class Wallet:
+
+    def __init__(self, balance):
+        self.__balance = balance   # private variable
+
+    def pay(self, amount):
+        if amount <= self.__balance:
+            self.__balance -= amount
+            print("Payment successful:", amount)
+        else:
+            print("Insufficient balance")
+
+    def show_balance(self):
+        print("Remaining balance:", self.__balance)
+
+
+# Polymorphism
+class UPI:
+    def pay(self, amount):
+        print("Paid", amount, "using UPI")
+
+
+class CreditCard:
+    def pay(self, amount):
+        print("Paid", amount, "using Credit Card")
+
+
+# Main Program
+
+# Encapsulation example
+wallet = Wallet(2000)
+wallet.pay(500)
+wallet.show_balance()
+
+print("-----")
+
+# Polymorphism example
+payment1 = UPI()
+payment2 = CreditCard()
+
+payment1.pay(300)
+payment2.pay(700)
+
+print("-----")
+
+# Inheritance + Abstraction example
+delivery1 = BikeDelivery("Anand")
+delivery2 = CarDelivery("Rahul")
+
+delivery1.deliver()
+delivery2.deliver()
